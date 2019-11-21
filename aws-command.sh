08b67d781b3b35db97607606779d00418fb5585f
Aidil Rezjki S S <13517070@std.stei.itb.ac.id>
2019-11-21 12:18:07 +0700

    Trying to fix.

--- aws-command.sh
+++ aws-command.sh
@@ -2,6 +2,7 @@ cd /home/ubuntu/ws-transaksi
 git stash
 git checkout master
 git pull origin master
+sudo npm install
 echo 'Deleting database screen...'
 screen -X -S wstransaction-database quit
 echo 'Deleting node screen...'
@@ -12,3 +13,4 @@ echo 'Entering database screen...'
 screen -d -m -S wstransaction-database sudo docker-compose up
 echo 'Entering node screen...'
 screen -d -m -S wstransaction-node node index.js
+echo 'Done'
