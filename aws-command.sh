28b836ed4f42487067dab1f79fdf4331e2266a1c
Aidil Rezjki S S <13517070@std.stei.itb.ac.id>
2019-11-21 12:09:04 +0700

    Final.

--- aws-command.sh
+++ aws-command.sh
@@ -2,12 +2,13 @@ cd /home/ubuntu/ws-transaksi
 git stash
 git checkout feature-CICD
 git pull origin feature-CICD
-echo 'Deleting screen...'
+echo 'Deleting database screen...'
 screen -X -S wstransaction-database quit
+echo 'Deleting node screen...'
 screen -X -S wstransaction-node quit
 echo 'Creating .env'
 cp ENV.SAMPLE .env
-echo 'Entering screen...'
+echo 'Entering database screen...'
 screen -d -m -S wstransaction-database sudo docker-compose up
-echo 'Entering second screen...'
+echo 'Entering node screen...'
 screen -d -m -S wstransaction-node node index.js
